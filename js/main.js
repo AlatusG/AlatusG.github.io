087a3d2d3b554c6f8ba9e0a089ad2455c44f32c0
Charles Cruz <ccruz.hiei@gmail.com>
2025-02-06 10:59:14 +1000

    added tabbed content with animation

---
+++ js/main.js
@@ -0,0 +1,19 @@
+function tabbedNav(event, tabName) {
+  let i, main_content, tabbed_btn;
+
+  // hide all .main_content elements
+  main_content = document.getElementsByClassName("main_content");
+  for (i = 0; i < main_content.length; i++) {
+    main_content[i].style.display = "none";
+  }
+
+  // hide .active from all .tabbed_btn elements
+  tabbed_btn = document.getElementsByClassName("tabbed_btn");
+  for (i = 0; i < tabbed_btn.length; i++) {
+    tabbed_btn[i].className = tabbed_btn[i].className.replace(" active", "");
+  }
+
+  // add .active to current tab and show content
+  document.getElementById(tabName).style.display = "flex";
+  event.currentTarget.className += " active";
+}
